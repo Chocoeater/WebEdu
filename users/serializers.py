60cc33c471345292d884cbdd5db032743b4a7275
Chocoeater <lonelypain98@gmail.com>
2025-07-15 23:49:41 +0300

    add history for users

--- users/serializers.py
+++ users/serializers.py
@@ -2,15 +2,17 @@ from rest_framework import serializers
 
 from users.models import User, Payment
 
-
-class UserSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = User
-        fields = '__all__'
-
 class PaymentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Payment
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    history_of_payments = PaymentSerializer(many=True, read_only=True, source='payments')
+
+    class Meta:
+        model = User
+        fields = ['email', 'avatar', 'phone', 'country', 'history_of_payments']
+
