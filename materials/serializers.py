95d67819402318bd7d5eb30486e3e35ff35668cb
Chocoeater <lonelypain98@gmail.com>
2025-07-15 22:51:48 +0300

    update ser for Course

--- materials/serializers.py
+++ materials/serializers.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 
 from materials.models import Course, Lesson
 
+class LessonSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Lesson
+        fields = '__all__'
 
 class CourseSerializer(serializers.ModelSerializer):
 
@@ -9,9 +14,16 @@ class CourseSerializer(serializers.ModelSerializer):
         model = Course
         fields = '__all__'
 
+class CourseRetrieveSerializer(serializers.ModelSerializer):
+    count_lessons = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
 
-class LessonSerializer(serializers.ModelSerializer):
+
+    def get_count_lessons(self, obj):
+        return obj.lessons.all().count()
 
     class Meta:
-        model = Lesson
-        fields = '__all__'
+        model = Course
+        fields = ('name', 'preview', 'description', 'count_lessons', 'lessons')
+
+
