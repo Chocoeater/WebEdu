3e0f036fdda1894b49969b5c4b1c4e2d3c4e2c0f
Chocoeater <lonelypain98@gmail.com>
2025-07-24 23:54:38 +0300

    add is_subscribed

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from materials.models import Course, Lesson
+from materials.models import Course, Lesson, Subscription
 from materials.validators import LinkValidator
 
 
@@ -21,11 +21,21 @@ class CourseSerializer(serializers.ModelSerializer):
 
 class CourseRetrieveSerializer(serializers.ModelSerializer):
     count_lessons = serializers.SerializerMethodField()
+    is_subscribed = serializers.SerializerMethodField()
     lessons = LessonSerializer(many=True, read_only=True)
 
     def get_count_lessons(self, obj):
         return obj.lessons.all().count()
 
+    def get_is_subscribed(self, obj):
+        request = self.context.get('request')
+        if request.user.is_authenticated:
+            return Subscription.objects.filter(
+                user=request.user,
+                course=obj
+            ).exists()
+        return False
+
     class Meta:
         model = Course
-        fields = ("name", "preview", "description", "count_lessons", "lessons")
+        fields = ("name", "preview", "description", "count_lessons", "lessons", "is_subscribed")
