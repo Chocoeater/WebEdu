67637fb07cf8c07af44fedaa85ac2306076a9abe
Chocoeater <lonelypain98@gmail.com>
2025-07-25 22:30:30 +0300

    update validator: null

--- materials/validators.py
+++ materials/validators.py
@@ -11,9 +11,13 @@ class LinkValidator:
         self.field = field
 
     def __call__(self, value):
+        tmp_val = value.get(self.field)
+
+        if not tmp_val:
+            return
+
         pattern = re.compile(
             r'(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})'
         )
-        tmp_val = value.get(self.field)
         if not pattern.match(tmp_val):
             raise serializers.ValidationError('Данная ссылка не поддерживается')
